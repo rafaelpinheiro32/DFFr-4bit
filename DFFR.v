ea41718ba04d474f4b039e81650f2c35ca432861
Rafael <151592512+rafaelpinheiro32@users.noreply.github.com>
2023-11-25 14:50:52 -0500

    Update DFFR.v
    Correcting comments

--- DFFR.v
+++ DFFR.v
@@ -1,14 +1,17 @@
 `timescale 1ns / 1ps
-\\ ECE230 DP3 EX 1
-\\ module definition of D-type flip flop
+// ECE230 DP3 EX 1
+// module definition of D-type flip flop
 
 module DFFr(d,clk,rst,q,qbar);
-input d, clk, rst;
-output reg q, qbar;
-always @ (posedge(clk), posedge(rst))
+input d, clk, rst; // inputs d, clock and reset
+output reg q, qbar; // outputs q and qbar
+
+always @ (posedge(clk), posedge(rst)) // always procedure at positive edge at clock or reset signals
+
 begin
 qbar = !q;
-if (rst == 1) q = 0;
-else q = d;
+if (rst == 1) q = 0; // if reset is high, q is 0
+else q = d; // if reset is low, q is d
 end
+
 endmodule
